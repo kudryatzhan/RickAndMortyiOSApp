799d71f3ffdd7137f5686f7572a2bfb6fc2d0d8e
Kudryatzhan Arziyev <karziyev@gmail.com>
2023-02-08 23:09:01 +0600

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Kudryatzhan Arziyev on 08.02.2023.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
