8ea5fbf645002a2985565dc298329844d22b34d8
Kudryatzhan Arziyev <karziyev@gmail.com>
2023-02-10 21:26:44 +0600

    Setup API Call

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 /// Controller to show and search for characters
 final class RMCharacterViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        RMService.shared.execute(.listCharactersRequest, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print("Total:" + String(model.info.pages))
+                print("Page result count: " + String(model.results.count))
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
     }
-
+    
 }
