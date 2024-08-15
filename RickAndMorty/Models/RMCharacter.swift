484b8ec85ba286b6fdfeb6928d6b15d4c4b61d63
Kudryatzhan Arziyev <karziyev@gmail.com>
2024-08-15 19:20:11 +0500

    feat: create model data types
    - Created types for character, episode and location

--- RickAndMorty/Models/RMCharacter.swift
+++ RickAndMorty/Models/RMCharacter.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+struct RMCharacter: Codable {
+    
+    enum Status: String, Codable {
+        case alive = "Alive"
+        case dead = "Dead"
+        case unknown
+    }
+    
+    enum Gender: String, Codable {
+        case female = "Female"
+        case male = "Male"
+        case genderless = "Genderless"
+        case unknown
+    }
+    
+    struct Origin: Codable {
+        let name: String
+        let url: String
+    }
+    
+    struct Location: Codable {
+        let name: String
+        let url: String
+    }
+    
+    let id: Int
+    let name: String
+    let status: Status
+    let species: String
+    let type: String
+    let gender: Gender
+    let origin: Origin
+    let location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+    
+}
